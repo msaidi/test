e2bb670819ef81cb8e035ef9ee5d506ed69b5171
msaidi <msaidi@users.noreply.github.com>
2014-04-09 17:01:37 +0200

    Update qsort.c

--- qsort.c
+++ qsort.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-
+void print(int *tab, int start, int end) {
+  for (int i = start; i <= end; i++) {
+    printf("%d ", tab[i]);
+  }
+  printf("\n");
+}
 
 int main() {
-  int tab[10] = {10,8,9,6,4,5,1,3,2,7};
+  int tab[10] = {1,2,3,4,5,6,7,8,9,10};
+  
 
-printf("0");
+ 
+  print(tab, 0, 9);
+
+
   return(0);
 }
